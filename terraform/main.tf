1fa8169ba6183aed6f254c27761cfb1b5284404a
Dmitry Valkov <dlcwalkoff@gmail.com>
2017-09-10 12:38:03 +0300

    Blueprint main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+provider "google" {
+    project = "my-otus"
+    region = "europe-west1"
+}
+
+resource "google_compute_instance" "app" {
+    name = "reddit-app"
+    machine_type = "g1-small"
+    zone = "europe-west1"
+
+    boot_disk {
+        initialize_params {
+            image = "reddit-base-1505035847"
+        }
+    }
+
+    network_interface {
+        network = "default"
+        access_config {}
+    }
+}

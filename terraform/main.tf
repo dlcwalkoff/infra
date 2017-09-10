96396db6495249d925657fa8d1bba845ae2e9575
Dmitry Valkov <dlcwalkoff@gmail.com>
2017-09-10 13:47:12 +0300

    Add firewall rights

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,7 @@ resource "google_compute_instance" "app" {
     name = "reddit-app"
     machine_type = "g1-small"
     zone = "europe-west1-b"
+    tags = ["reddit-app"]
 
     metadata {
         sshKeys = "appuser:${"~/.ssh/appuser.pub"}"
@@ -22,4 +23,15 @@ resource "google_compute_instance" "app" {
         network = "default"
         access_config {}
     }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+    name = "allow-puma-default"
+    network = "default"
+    allow {
+        protocol = "tcp"
+        ports = ["9292"]
+    }
+    source_ranges = ["0.0.0.0/0"]
+    target_tags = ["reddit-app"]
 }

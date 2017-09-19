a065d08e0285bf6d39b7d96941a8dc2d09ae3708
Dmitry Valkov <dlcwalkoff@gmail.com>
2017-09-19 14:54:49 +0700

    Add and use private_key_path var

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,6 +10,11 @@ variable region {
 variable public_key_path {
     description = "Path to the public key used for ssh access"
 }
+}
+
+variable private_key_path {
+    description = "Path to the privare key used for ssh access"
+}
 
 variable disk_image {
     description = "Disk image"

9ad92cdf1dc591493729d0e18a528443958e3ed1
Dmitry Valkov <dlcwalkoff@gmail.com>
2017-10-05 20:09:08 +0300

    Add socket test

--- ansible/roles/db/molecule/default/tests/test_default.py
+++ ansible/roles/db/molecule/default/tests/test_default.py
@@ -9,7 +9,11 @@ def test_config_file(host):
     assert config_file.contains('bindIp: 0.0.0.0')
     assert config_file.is_file
 
-def test_db_port(host):
+def test_db_port_file(host):
     config_file = host.file('/etc/mongod.conf')
     assert config_file.contains('port: 27017')
     assert config_file.is_file
+
+def test_db_port_socket(host):
+    socket = host.socket('tcp://0.0.0.0:27017');
+    assert socket.is_listening;
